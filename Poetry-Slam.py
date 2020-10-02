5aabefb3d02aed25e55eee1b18744e92b5c0f414
omarlopez2001 <omarlopez9445@gmail.com>
2020-10-01 23:09:15 -0400

    Added function
    I added a function called lines_printed_custom. This function prints out every even line from the poem.

--- Poetry-Slam.py
+++ Poetry-Slam.py
@@ -9,7 +9,7 @@ def get_file_lines(filename):
 def lines_printed_backwards(lines_list):
     lines_list = open("poem.txt", "r").readlines()
     num_of_lines = len(lines_list)
-    lines_list = lines_list.reverse()
+    lines_list = lines_list[:: -1]
 
     for line in range(num_of_lines):
         reverse = str(num_of_lines - line) + "" + lines_list[line]
@@ -21,5 +21,15 @@ def lines_printed_random(lines_list):
     
     for line in lines_list:
         line = random.randrange(num_of_lines)
-        random = str(line + 1) + "" +lines_list[line]
-        print(random)
+        random_order = str(line + 1) + "" + lines_list[line]
+        print(random_order)
+
+def lines_printed_custom(lines_list):
+    lines_list = open("poem.txt", "r").readlines()
+    num_of_lines = len(lines_list)
+    i = 1
+
+    for i in range(num_of_lines):
+        if i % 2 == 0:
+            print(i,lines_list[i])
+        i += 1
