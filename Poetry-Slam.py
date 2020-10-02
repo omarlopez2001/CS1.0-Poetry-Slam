f04f194bfa028ea7e2ce2970fccc8ebb19183cb5
omarlopez2001 <omarlopez9445@gmail.com>
2020-10-01 20:10:56 -0400

    Added another function
    I added another function. This function is called lines_printed_backwards and that's what it does, it prints the lines of the poem backwards. A for loop is also implemented to reverse the line numbers.

--- Poetry-Slam.py
+++ Poetry-Slam.py
@@ -1,5 +1,14 @@
 def get_file_lines(filename):
-    poem = "Poem.txt"
+    poem = "poem.txt"
     infile = open(poem, "r").readlines()
     infile.close()
     return infile
+
+def lines_printed_backwards(lines_list):
+    lines_list = open("poem.txt", "r").readlines()
+    num_of_lines = len(lines_list)
+    lines_list = lines_list.reverse()
+
+    for line in range(num_of_lines):
+        reverse = str(num_of_lines - line) + "" + lines_list[line]
+        print(reverse)
